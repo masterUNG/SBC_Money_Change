9b87bee4eee2b9b4e79e0dfeae50427aab2d2cac
MasterUNG <phrombutr@gmail.com>
2018-01-12 16:05:58 +0700

    Finish 12Jan

---
+++ app/src/main/java/masterung/androidthai/in/th/scbmoneychange/fragment/MainFragment.java
@@ -0,0 +1,28 @@
+package masterung.androidthai.in.th.scbmoneychange.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import masterung.androidthai.in.th.scbmoneychange.R;
+
+/**
+ * Created by masterung on 12/1/2018 AD.
+ */
+
+public class MainFragment extends Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return view;
+    }
+}   // Main Class
