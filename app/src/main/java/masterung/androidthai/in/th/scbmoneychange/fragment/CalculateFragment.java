0268582ac3f17bab90258e2541ad17e89490d6f6
MasterUNG <phrombutr@gmail.com>
2018-01-13 11:50:14 +0700

    Create Toolbar Success

--- app/src/main/java/masterung/androidthai/in/th/scbmoneychange/fragment/CalculateFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/scbmoneychange/fragment/CalculateFragment.java
@@ -3,10 +3,12 @@ package masterung.androidthai.in.th.scbmoneychange.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import masterung.androidthai.in.th.scbmoneychange.MainActivity;
 import masterung.androidthai.in.th.scbmoneychange.R;
 
 /**
@@ -14,6 +16,31 @@ import masterung.androidthai.in.th.scbmoneychange.R;
  */
 
 public class CalculateFragment extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Toolbar Controller
+        Toolbar toolbar = getView().findViewById(R.id.toolbarCalculate);
+        ((MainActivity)getActivity()).setSupportActionBar(toolbar);
+
+        ((MainActivity) getActivity()).getSupportActionBar().setTitle(getString(R.string.th_exchange));
+        ((MainActivity) getActivity()).getSupportActionBar().setSubtitle(getString(R.string.th_sub_exchange));
+
+        ((MainActivity) getActivity()).getSupportActionBar().setHomeButtonEnabled(true);
+        ((MainActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().popBackStack();
+            }
+        });
+
+
+    }   // Main Method
 
     @Nullable
     @Override
