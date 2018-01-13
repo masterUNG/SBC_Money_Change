8a4e643d79176201ab1658a2edc94fa095bc1b63
MasterUNG <phrombutr@gmail.com>
2018-01-13 10:46:31 +0700

    Replace Fragment Success

--- app/src/main/java/masterung/androidthai/in/th/scbmoneychange/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/scbmoneychange/fragment/MainFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import masterung.androidthai.in.th.scbmoneychange.R;
 
@@ -14,6 +15,32 @@ import masterung.androidthai.in.th.scbmoneychange.R;
  */
 
 public class MainFragment extends Fragment{
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Calculate Controller
+        Button button = getView().findViewById(R.id.btnCalculate);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+//                Replace Fragment
+                getActivity().getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.contentMainFragment, new CalculateFragment())
+                        .addToBackStack(null)
+                        .commit();
+
+
+            }
+        });
+
+
+
+    }   // Main Method
 
     @Nullable
     @Override
